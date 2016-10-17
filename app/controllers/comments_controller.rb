7d0b78967a9f5d282c5352e5b51241c1a115aa38
Latoya Watson <latoyamwatson@gmail.com>
2016-10-17 11:21:12 -0400

    adding comments model to article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+
+  def new
+    @post = Post.find params[:post_id]
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find params [:post_id]
+    @comment = @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find params[:post_id]
+    @comment = Comment.find params[:id]
+  end
+
+  private
+    def comment_params
+      parmas.require(:comment).permit(:title, :body)
+    end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,43 @@ end
 
 def show
   @post = Post.find params[:id]
+  @comment = Comment.find params[:id]
 end
 
+def edit
+  @post = Post.find params[:id]
+end
 
+def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
+  end
+end
+
+def update
+  @post = Post.find params[:id]
+
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @post = Post.find params[:id]
+  @post.destroy
+
+  redirect_to posts_path
+end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
 end
