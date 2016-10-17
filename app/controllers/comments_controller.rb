765629ba4e72a253633fedd8736c21fbf37b744d
Latoya Watson <latoyamwatson@gmail.com>
2016-10-17 14:24:07 -0400

    last commit before dropping db and loading data

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
 
+  def show
+    @post = Post.find params[:post_id]
+    @comment = Comment.find params[:id]
+  end
+
   def create
     @post = Post.find params[:post_id]
     @comment = @post.comments.create(comment_params)
@@ -11,9 +16,16 @@ class CommentsController < ApplicationController
     @comment = Comment.find params[:id]
   end
 
+  def destroy
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.find params[:id]
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:title, :body)
     end
-
 end
