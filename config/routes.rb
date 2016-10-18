fcce79c6d1fed86c47a7f084dced50ea920796d5
Latoya Watson <latoyamwatson@gmail.com>
2016-10-18 10:25:13 -0400

    voting works as expected - will merge with master

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     member do
-      put "like", to: "links#upvote"
-      put "dislike", to: "links#downvote"
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
     end
     resources :comments
     end
