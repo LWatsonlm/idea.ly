72431ce3ee3b7ac2ef0ff046c6bff6d3d20d314f
Latoya Watson <latoyamwatson@gmail.com>
2016-10-18 20:58:39 -0400

    re-seeded, data is clean, looking good

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Post.destroy_all
 Comment.destroy_all
+User.destroy_all
 
-Post.create!(title: "Thinking about becoming a writer", body: "What type of fanfic do you like? Would you read my Ed, Edd, n Eddy fanfic?")
-Post.create!(title: "What's your favorite food fusion?", body: "Me and my buddies are thinking of opening a restaurant. Thinking of possible types of restaurants. What Mexican and Asian? Indian and American? or something completeley different like sushi burritos?")
-Post.create!(title: "Bikers!! What would be helpful to you?", body: "Calling all D.C. bicyclists! I'm in the middle of customer discovery for my new product. As a city commuter, what would be helpful for you?")
-Post.create!(title: "Should I become a front end developer, or a back end developer?", body: "Thinking about switching careers but having a hard time. Can anyone give me feedback on either career path?")
+latoya = User.create!(email: "latoya@gmail.com", password: "123456")
+eric = User.create!(email: "eric@gmail.com", password: "123456")
 
+writer = Post.create!(title: "Thinking about becoming a writer", topic: "Idea", user: eric, body: "What type of fanfic do you like? Would you read my Ed, Edd, n Eddy fanfic?")
+food = Post.create!(title: "What's your favorite food fusion?", topic: "Question", user: latoya, body: "Me and my buddies are thinking of opening a restaurant. Thinking of possible types of restaurants. What Mexican and Asian? Indian and American? or something completeley different like sushi burritos?")
+biking = Post.create!(title: "Bikers!! What would be helpful to you?", topic: "Queston", user: latoya, body: "Calling all D.C. bicyclists! I'm in the middle of customer discovery for my new product. As a city commuter, what would be helpful for you?")
+career = Post.create!(title: "Should I become a front end developer, or a back end developer?", user: eric, topic: "Question", body: "Thinking about switching careers but having a hard time. Can anyone give me feedback on either career path?")
+random = Post.create!(title: "OMG, you guys!", topic: "Random Thought", user: latoya, body: "Wrong is spelled wrong in the dictionary! #MindBlown!")
 
-Comment.create!(title: "biker, here!", body: "Yeah, I love biking in D.C. It would be helpful to know which streets to avoid because there isn't any bike lanes", post_id: 3)
-Comment.create!(title: "yum! I'm hungry!", body: "I LOVE FOOD. Honestly, I would try to think out of the box, something that hasn't been created yet. Pizza Sushi?, Chicken Cupcakes?", post_id: 2)
-Comment.create!(title: "Hmmm...", body: "It all depends on what you like to do. Try some courses on Code Academy or Treehouse and let us know what you liked.", post_id: 4)
-Comment.create!(title: "Not a fan", body: "Ed, Edd, n Eddy? Nah, not a fan. Maybe try writing about something more relevant. Like Trump fanfiction... however, it would be super hard to know if it's real or not", post_id: 1)
+Comment.create!(title: "Not a fan", body: "Ed, Edd, n Eddy? Nah, not a fan. Maybe try writing about something more relevant. Like Trump fanfiction... however, it would be super hard to know if it's real or not", post: writer)
+Comment.create!(title: "yum! I'm hungry!", body: "I LOVE FOOD. Honestly, I would try to think out of the box, something that hasn't been created yet. Pizza Sushi?, Chicken Cupcakes?", post: food)
+Comment.create!(title: "biker, here!", body: "Yeah, I love biking in D.C. It would be helpful to know which streets to avoid because there isn't any bike lanes", post: biking)
+Comment.create!(title: "Hmmm...", body: "It all depends on what you like to do. Try some courses on Code Academy or Treehouse and let us know what you liked.", post: career)
+Comment.create!(title: "Whooooa!!", body: "It sure is! haha!", post: random)
